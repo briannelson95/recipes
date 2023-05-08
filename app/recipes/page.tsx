0bebec0e0d450c0d8d4ab8a265fe78bc642c1a0f
briannelson95 <71141646+briannelson95@users.noreply.github.com>
2023-05-08 10:59:47 -0400

    shuffle recipes

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -7,13 +7,26 @@ import React from 'react'
 export const revalidate = 500;
 
 export default async function Recipes() {
-    const allRecipes = await client.fetch(recipes)
+    const allRecipes = await client.fetch(recipes);
+
+    const shuffleArr = (array: any[]) => {
+        let currentIndex = array.length, randomIndex;
+
+        while (currentIndex != 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    };
 
     return (
         <div className='mx-2 max-w-6xl md:mx-auto'>
             Recipes
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
-                {allRecipes.map((item: any, index: any) => (
+                {shuffleArr(allRecipes).map((item: any, index: any) => (
                     <RecipeCard 
                         key={index}
                         title={item.title}
